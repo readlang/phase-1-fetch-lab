f77e24725c1c65cf3ad387c45a68c68e67664de6
Read Langworthy <rlangworthy@gmail.com>
2021-10-22 17:27:06 -0700

    commit tree f18fdc7e8119871242fed77ab7b710401701e847

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
   
+  return(
+    fetch("https://anapioficeandfire.com/api/books")
+    .then(function (response) {
+      return response.json()
+    })
+    //.then((json) => (renderBooks(json) ))
+    .then(function (json) {
+      renderBooks(json);
+      return json
+    })
+  
+  )
+  
 }
 
 function renderBooks(books) {
